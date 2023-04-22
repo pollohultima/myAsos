95a596c6698ff4643dacef068df593799bc2efba
pollohultima <matteo.polonio@gmail.com>
2023-04-22 22:00:04 +0200

    basic structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import LandingPageView from "../views/LandingPageView";
+import ShopView from "../views/ShopView";
+import CheckoutView from "../views/CheckoutView";
+import PaymentCompletedView from "../views/PaymentCompletedView";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: LandingPageView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/shop",
+      name: "shop",
+      component: ShopView,
+    },
+    {
+      path: "/shop/checkout",
+      name: "checkout",
+      component: CheckoutView,
+    },
+    {
+      path: "/shop/checkout/success",
+      name: "success",
+      component: PaymentCompletedView,
+    },
+  ],
+});
 
-export default router
+export default router;
